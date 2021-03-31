e75f2613fef6839c59e2c3fa8906a95d9377e772
Shuaib <shuaibmj369@gmail.com>
2021-03-31 11:00:10 +0530

    adding javascript

---
+++ javascriptprograms/languagefundamentals/languagefun.js
@@ -0,0 +1,16 @@
+var name = "Shuaib ";
+var age =" 25 years old";
+console.log("hai am " +name +age)
+// second method
+var name = "Shuaib ";
+var age =" 25 ";
+console.log("hai am "+name+" "+age+" years old ")
+
+//our company name luminar is located in Kakkanad 
+
+var company=" luminar"
+var place=" Kakkanad "
+console.log("our company name"+company+" located in"+place)
+
+//second method 
+console.log(`our company ${company} located in ${place}`);
---
+++ javascriptprograms/languagefundamentals/swapping.js
@@ -0,0 +1,10 @@
+var num1=10;
+var num2=20;
+
+//swapping 
+num1= num1+num2 // 10+20=30
+num2=num1-num2  // 30-20=10
+num1=num1-num2  // 30-10=20
+
+console.log(`Num 1 ${num1}`);
+console.log(`Num 2 ${num2}`);
